7a52a16af15a2bd0c89d715ab82ba9817cbdbd74
Kouser Islam <161111851+kouser-github@users.noreply.github.com>
2024-10-21 00:50:54 +0530

    Create lagger.py

---
+++ lagger.py
@@ -0,0 +1,17 @@
+import os
+import sys
+a = 1
+b = 1
+sys.set_int_max_str_digits(0)
+while 1 !=2 :
+    c = a*b
+    g = 0
+    print(c*d*e*f*g*h)
+    os.system("clear")
+    d = a*b*a*b
+    e = d*d
+    f = e*e
+    g = f*f
+    h = g*g
+    a = a+b
+    b = a
